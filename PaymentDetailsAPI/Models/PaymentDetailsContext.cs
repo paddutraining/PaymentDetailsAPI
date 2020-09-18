31ad93f2f0cd73d10b4c1cdbdb85fb1185a1f6fe
sudhakar <sudhar@DESKTOP-HEEIV1D>
2020-09-18 11:51:20 +0530

    Add project files.

---
+++ PaymentDetailsAPI/Models/PaymentDetailsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentDetailsAPI.Models
+{
+    public class PaymentDetailsContext:DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions<PaymentDetailsContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+       
+    }
+}
